73d8897a0f30b6921ed2d7472d97b6740c19dfcb
Alvaro Muñoz Pavón <alvaro.munoz.pavon@alumnos.upm.es>
2025-04-08 19:56:15 +0200

    Fixed

--- src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
+++ src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,6 @@ import es.upm.dit.isst.tfgapi.repository.SesionRepository;
 import es.upm.dit.isst.tfgapi.repository.TFGRepository;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.transaction.Transactional;
 
 @RestController
